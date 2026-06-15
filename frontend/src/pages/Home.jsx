1e90dc7780f864e998d693df3d30c0677a482897
Kyle Dudley <kbriandudley@gmail.com>
2026-06-15 14:20:29 +0200

    edited text in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div style={{ padding: '20px' }}>
-      <h1 style={{backgroundColor: "purple"}}>Kyle Dudley</h1>
+      <h1 style={{backgroundColor: "purple"}}>DataFriendz</h1>
       <h2>Welcome to my guestbook</h2>
         <p>Click 'Submit' above to sign!!</p>
     </div>
